4b195c80c00c356e44e8b66acb64c9d71d53d4f0
Ivan Mikhailov <meehighlov@MacBook-Air-2.local>
2025-03-16 00:50:41 +0300

    fix: mute migration

--- migrations/20250315000000_add_silent_notifications.sql
+++ migrations/20250315000000_add_silent_notifications.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE chat ADD COLUMN silent_notifications INTEGER DEFAULT 1;
+ALTER TABLE chat ADD COLUMN silent_notifications INTEGER DEFAULT 0;
 -- +goose StatementEnd
 
 -- +goose Down
