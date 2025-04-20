c79b57ac8ba6c4ee325d77c836ebb861ece16c94
Ivan Mikhailov <meehighlov@MacBook-Air-2.local>
2025-04-21 02:53:13 +0300

    fix: same

--- migrations/20250701000001_update_timestamps.sql
+++ migrations/20250701000001_update_timestamps.sql
@@ -3,14 +3,14 @@
 -- Обновляем таблицу user
 ALTER TABLE "user" 
     ALTER COLUMN created_at TYPE TIMESTAMP WITH TIME ZONE USING CASE 
-        WHEN created_at ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
-            to_timestamp(created_at, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
+        WHEN created_at::text ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
+            to_timestamp(created_at::text, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
         ELSE 
             created_at::TIMESTAMP WITH TIME ZONE 
     END,
     ALTER COLUMN updated_at TYPE TIMESTAMP WITH TIME ZONE USING CASE 
-        WHEN updated_at ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
-            to_timestamp(updated_at, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
+        WHEN updated_at::text ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
+            to_timestamp(updated_at::text, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
         ELSE 
             updated_at::TIMESTAMP WITH TIME ZONE 
     END;
@@ -18,14 +18,14 @@ ALTER TABLE "user"
 -- Обновляем таблицу chat
 ALTER TABLE "chat" 
     ALTER COLUMN created_at TYPE TIMESTAMP WITH TIME ZONE USING CASE 
-        WHEN created_at ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
-            to_timestamp(created_at, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
+        WHEN created_at::text ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
+            to_timestamp(created_at::text, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
         ELSE 
             created_at::TIMESTAMP WITH TIME ZONE 
     END,
     ALTER COLUMN updated_at TYPE TIMESTAMP WITH TIME ZONE USING CASE 
-        WHEN updated_at ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
-            to_timestamp(updated_at, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
+        WHEN updated_at::text ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
+            to_timestamp(updated_at::text, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
         ELSE 
             updated_at::TIMESTAMP WITH TIME ZONE 
     END;
@@ -33,14 +33,14 @@ ALTER TABLE "chat"
 -- Обновляем таблицу friend
 ALTER TABLE "friend" 
     ALTER COLUMN created_at TYPE TIMESTAMP WITH TIME ZONE USING CASE 
-        WHEN created_at ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
-            to_timestamp(created_at, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
+        WHEN created_at::text ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
+            to_timestamp(created_at::text, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
         ELSE 
             created_at::TIMESTAMP WITH TIME ZONE 
     END,
     ALTER COLUMN updated_at TYPE TIMESTAMP WITH TIME ZONE USING CASE 
-        WHEN updated_at ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
-            to_timestamp(updated_at, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
+        WHEN updated_at::text ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
+            to_timestamp(updated_at::text, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
         ELSE 
             updated_at::TIMESTAMP WITH TIME ZONE 
     END;
@@ -48,14 +48,14 @@ ALTER TABLE "friend"
 -- Обновляем таблицу wish
 ALTER TABLE "wish" 
     ALTER COLUMN created_at TYPE TIMESTAMP WITH TIME ZONE USING CASE 
-        WHEN created_at ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
-            to_timestamp(created_at, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
+        WHEN created_at::text ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
+            to_timestamp(created_at::text, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
         ELSE 
             created_at::TIMESTAMP WITH TIME ZONE 
     END,
     ALTER COLUMN updated_at TYPE TIMESTAMP WITH TIME ZONE USING CASE 
-        WHEN updated_at ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
-            to_timestamp(updated_at, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
+        WHEN updated_at::text ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
+            to_timestamp(updated_at::text, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
         ELSE 
             updated_at::TIMESTAMP WITH TIME ZONE 
     END;
@@ -63,14 +63,14 @@ ALTER TABLE "wish"
 -- Обновляем таблицу wish_list
 ALTER TABLE "wish_list" 
     ALTER COLUMN created_at TYPE TIMESTAMP WITH TIME ZONE USING CASE 
-        WHEN created_at ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
-            to_timestamp(created_at, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
+        WHEN created_at::text ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
+            to_timestamp(created_at::text, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
         ELSE 
             created_at::TIMESTAMP WITH TIME ZONE 
     END,
     ALTER COLUMN updated_at TYPE TIMESTAMP WITH TIME ZONE USING CASE 
-        WHEN updated_at ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
-            to_timestamp(updated_at, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
+        WHEN updated_at::text ~ '^\d{2}\.\d{2}\.\d{4}T\d{2}:\d{2}:\d{2}$' THEN 
+            to_timestamp(updated_at::text, 'DD.MM.YYYY"T"HH24:MI:SS') AT TIME ZONE 'UTC'
         ELSE 
             updated_at::TIMESTAMP WITH TIME ZONE 
     END;
